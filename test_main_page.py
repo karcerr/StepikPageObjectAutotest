d75827483d814f2f2c9b19d4246fb030006a32fc
karcerr <roma.mershon@mail.ru>
2025-04-14 18:45:02 +0500

    add 4.3.10

--- test_main_page.py
+++ test_main_page.py
@@ -48,18 +48,19 @@ def test_promo_page_4_3_4(browser, link):
     page.added_price_correct()
     time.sleep(2)
 
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
-def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
-
-def test_guest_should_see_login_link(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 def test_login_page(browser):
     page = LoginPage(browser, login_link)
     page.open()
